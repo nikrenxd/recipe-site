2349354b0b44830060cf93235359d53dd5fe8f46
nikrenxd <nikrenc1101@gmail.com>
2023-11-02 05:01:11 +0200

    Bad email verification

--- auths/urls.py
+++ auths/urls.py
@@ -1,29 +1,15 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import SignupView
+from .views import SignupView, ConfirmEmailView, ExpiredEmailView
 
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path(
-        "password_reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="password_reset",
+        "activate/<int:id>/<str:token>/",
+        ConfirmEmailView.as_view(),
+        name="activate",
     ),
-    path(
-        "password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
+    path("activate/expired/", ExpiredEmailView.as_view(), name="expired"),
 ]
--- auths/views.py
+++ auths/views.py
@@ -1,14 +1,71 @@
-from django.views.generic import FormView
+from datetime import datetime, timedelta
+from django.views.generic import FormView, TemplateView
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404, redirect
+from django.core.mail import send_mail
+from django.utils.http import base36_to_int
+from django.contrib.auth.tokens import default_token_generator
 
 from users.forms import CustomUserCreationForm
+from users.models import CustomUser
 
 
 class SignupView(FormView):
     form_class = CustomUserCreationForm
+    model = CustomUser
     template_name = "registration/signup.html"
     success_url = reverse_lazy("login")
 
     def form_valid(self, form):
+        form.instance.is_active = False
         form.save()
+        token = default_token_generator.make_token(form.instance)
+        activation_link = self.request.build_absolute_uri(
+            reverse_lazy(
+                "activate",
+                kwargs={
+                    "id": form.instance.id,
+                    "token": token,
+                },
+            )
+        )
+        send_mail(
+            "Account activation",
+            f"Here is your link\n{activation_link}",
+            "admin@mail.com",
+            [form.instance.email],
+        )
+
         return super().form_valid(form)
+
+
+class ConfirmEmailView(TemplateView):
+    template_name = "confirmation/confirm_success_page.html"
+
+    def get(self, request, *args, **kwargs):
+        token = kwargs["token"]
+        user_id = kwargs["id"]
+        user = None
+
+        timestamp = base36_to_int(token.split("-")[0])
+        timestamp_minutes = datetime.fromtimestamp(timestamp).minute
+        now_time = datetime.now().minute
+
+        try:
+            user = CustomUser.objects.get(id=user_id)
+        except CustomUser.DoesNotExist:
+            return redirect("home")
+
+        if (now_time - timestamp_minutes) < 5 and user is not None:
+            if default_token_generator.check_token(user, token):
+                user.is_active = True
+                user.save()
+        else:
+            user.delete()
+            return redirect("expired")
+
+        return super().get(request, *args, **kwargs)
+
+
+class ExpiredEmailView(TemplateView):
+    template_name = "confirmation/token_expired.html"
